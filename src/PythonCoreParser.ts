ede3203bdb342c1969826086123a391f493e0d5a
stenbror <stenbror@hotmail.com>
2021-01-23 00:19:04 +0100

    Moved Token kinds out to Token module.

--- src/PythonCoreParser.ts
+++ src/PythonCoreParser.ts
@@ -1,114 +1,13 @@
+import { Token, TokenKind, StringLiteral, NameLiteral, NumberLiteral } from "./Token";
 import { ASTNode } from "./ast/ASTNode";
 
-class SyntaxErrorException extends Error {
-    private Position: number;
-    private ErrorToken: Token;
 
-    constructor(pos: number, text: string, token: Token) {
+class SyntaxErrorException extends Error {
+    constructor(private Position: number, private text: string, private Token: Token) {
         super(text);
-        this.Position = pos;
-        this.ErrorToken = token;
         Object.setPrototypeOf(this, SyntaxErrorException.prototype);
     }
 }
-
-
-enum TriviaKind {
-    Empty, WhiteSpace, NewLine, LineContinuation, Comment
-}
-
-class Trivia {
-    private startPosition: number;
-    private endPosition: number;
-    private kind: TriviaKind;
-
-    constructor(startPos: number, endPos: number, kind: TriviaKind) {
-        this.startPosition = startPos;
-        this.endPosition = endPos;
-        this.kind = kind;
-    }
-}
-
-enum TokenKind {
-    Empty, EOF, Newline, Indent, Dedent, Py_False, Py_None, Py_True, Py_And, Py_As, Py_Assert, 
-    Py_Async, Py_Await, Py_Break, Py_Class, Py_Continue, Py_Def, PyDel, Py_Elif, Py_Else, Py_Except, 
-    Py_Finally, Py_For, Py_From, Py_Global, Py_If, Py_Import, Py_In, Py_Is, Py_Lambda, Py_Nonlocal,
-    Py_Not, Py_Or, Py_Pass, Py_Raise, Py_Return, Py_Try, Py_While, Py_With, Py_Yield, Py_Plus, Py_Minus, 
-    Py_Mul, Py_Power, Py_Div, Py_FloorDiv, Py_Modulo, Py_Matrice, Py_ShiftLeft, Py_ShiftRight, Py_BitAnd,
-    Py_BitOr, Py_BitXor, Py_BitInvert, Py_Less, Py_Greater, Py_LessEqual, Py_GreaterEqual, Py_Equal, 
-    Py_NotEqual, Py_LeftParen, Py_RightParen, Py_LeftBracket, Py_RightBracket, Py_LeftCurly, Py_RightCurly, 
-    Py_Comma, Py_Colon, Py_ColonAssign, Py_Dot, Py_Elipsis, Py_SemiColon, Py_Assign, Py_Arrow, Py_PlusAssign,
-    Py_MinusAssign, Py_MulAssign, Py_PowerAssign, Py_DivAssign, Py_FloorDivAssign, Py_ModuloAssign, 
-    Py_MatriceAssign, Py_BitAndAssign, Py_BitOrAssign, Py_BitXorAssign, Py_ShiftLeftAssign, Py_ShiftRightAssign, 
-    Name, Number, String, TypeComment
-}
-
-class Token {
-    private startPosition: number;
-    private endPosition: number;
-    private kind: TokenKind;
-    private trivias: Trivia[];
-
-    constructor(startPos: number, endPos: number, kind: TokenKind, trivias: Trivia[]) {
-        this.startPosition = startPos;
-        this.endPosition = endPos;
-        this.kind = kind;
-        this.trivias = trivias;
-    }
-
-    getStartPosition() : number {
-        return this.startPosition;
-    }
-
-    getEndPosition() : number {
-        return this.endPosition;
-    }
-
-    getKind() : TokenKind {
-        return this.kind;
-    }
-}
-
-class NameLiteral extends Token {
-    private name: string;
-
-    constructor(startPos: number, endPos: number, trivias: Trivia[], name: string) {
-        super(startPos, endPos, TokenKind.Name, trivias);
-        this.name = name;
-    }
-}
-
-class NumberLiteral extends Token {
-    private literal: string;
-
-    constructor(startPos: number, endPos: number, trivias: Trivia[], literal: string) {
-        super(startPos, endPos, TokenKind.Number, trivias);
-        this.literal = literal;
-    }
-}
-
-class StringLiteral extends Token {
-    private literal: string;
-
-    constructor(startPos: number, endPos: number, trivias: Trivia[], literal: string) {
-        super(startPos, endPos, TokenKind.String, trivias);
-        this.literal = literal;
-    }
-}
-
-class TypeComment extends Token {
-    private comment: string;
-
-    constructor(startPos: number, endPos: number, trivias: Trivia[], comment: string) {
-        super(startPos, endPos, TokenKind.TypeComment, trivias);
-        this.comment = comment;
-    }
-}
-
-
-
-
-
 
 
 
---
+++ src/Token.ts
@@ -0,0 +1,92 @@
+
+export enum TriviaKind {
+    Empty, WhiteSpace, NewLine, LineContinuation, Comment
+}
+
+export class Trivia {
+    private startPosition: number;
+    private endPosition: number;
+    private kind: TriviaKind;
+
+    constructor(startPos: number, endPos: number, kind: TriviaKind) {
+        this.startPosition = startPos;
+        this.endPosition = endPos;
+        this.kind = kind;
+    }
+}
+
+export enum TokenKind {
+    Empty, EOF, Newline, Indent, Dedent, Py_False, Py_None, Py_True, Py_And, Py_As, Py_Assert, 
+    Py_Async, Py_Await, Py_Break, Py_Class, Py_Continue, Py_Def, PyDel, Py_Elif, Py_Else, Py_Except, 
+    Py_Finally, Py_For, Py_From, Py_Global, Py_If, Py_Import, Py_In, Py_Is, Py_Lambda, Py_Nonlocal,
+    Py_Not, Py_Or, Py_Pass, Py_Raise, Py_Return, Py_Try, Py_While, Py_With, Py_Yield, Py_Plus, Py_Minus, 
+    Py_Mul, Py_Power, Py_Div, Py_FloorDiv, Py_Modulo, Py_Matrice, Py_ShiftLeft, Py_ShiftRight, Py_BitAnd,
+    Py_BitOr, Py_BitXor, Py_BitInvert, Py_Less, Py_Greater, Py_LessEqual, Py_GreaterEqual, Py_Equal, 
+    Py_NotEqual, Py_LeftParen, Py_RightParen, Py_LeftBracket, Py_RightBracket, Py_LeftCurly, Py_RightCurly, 
+    Py_Comma, Py_Colon, Py_ColonAssign, Py_Dot, Py_Elipsis, Py_SemiColon, Py_Assign, Py_Arrow, Py_PlusAssign,
+    Py_MinusAssign, Py_MulAssign, Py_PowerAssign, Py_DivAssign, Py_FloorDivAssign, Py_ModuloAssign, 
+    Py_MatriceAssign, Py_BitAndAssign, Py_BitOrAssign, Py_BitXorAssign, Py_ShiftLeftAssign, Py_ShiftRightAssign, 
+    Name, Number, String, TypeComment
+}
+
+export class Token {
+    private startPosition: number;
+    private endPosition: number;
+    private kind: TokenKind;
+    private trivias: Trivia[];
+
+    constructor(startPos: number, endPos: number, kind: TokenKind, trivias: Trivia[]) {
+        this.startPosition = startPos;
+        this.endPosition = endPos;
+        this.kind = kind;
+        this.trivias = trivias;
+    }
+
+    getStartPosition() : number {
+        return this.startPosition;
+    }
+
+    getEndPosition() : number {
+        return this.endPosition;
+    }
+
+    getKind() : TokenKind {
+        return this.kind;
+    }
+}
+
+export class NameLiteral extends Token {
+    private name: string;
+
+    constructor(startPos: number, endPos: number, trivias: Trivia[], name: string) {
+        super(startPos, endPos, TokenKind.Name, trivias);
+        this.name = name;
+    }
+}
+
+export class NumberLiteral extends Token {
+    private literal: string;
+
+    constructor(startPos: number, endPos: number, trivias: Trivia[], literal: string) {
+        super(startPos, endPos, TokenKind.Number, trivias);
+        this.literal = literal;
+    }
+}
+
+export class StringLiteral extends Token {
+    private literal: string;
+
+    constructor(startPos: number, endPos: number, trivias: Trivia[], literal: string) {
+        super(startPos, endPos, TokenKind.String, trivias);
+        this.literal = literal;
+    }
+}
+
+export class TypeComment extends Token {
+    private comment: string;
+
+    constructor(startPos: number, endPos: number, trivias: Trivia[], comment: string) {
+        super(startPos, endPos, TokenKind.TypeComment, trivias);
+        this.comment = comment;
+    }
+}
