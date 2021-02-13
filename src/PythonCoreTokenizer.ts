59f60ca0ac83f00423ace86594a8a56674f607db
stenbror <stenbror@hotmail.com>
2021-02-13 14:43:50 +0100

    Started on Tokenizer.

---
+++ src/PythonCoreTokenizer.ts
@@ -0,0 +1,16 @@
+
+// PythonCoreTokenizer
+// Written by Richard Magnor Stenbro. stenbror@hotmail.com
+// Implements Python grammar 3.9 in Typescript for code analyzing purposes of Python code.
+// Copyright (C) 2021 By Richard Magnor Stenbro. Free to use for any non profit purposes.
+
+import { Token, TokenKind, StringLiteral, NameLiteral, NumberLiteral } from "./Token";
+
+class PythonCoreTokenizer {
+
+    constructor(private SourceCode: string) {
+        
+    }
+}
+
+export default PythonCoreTokenizer;
