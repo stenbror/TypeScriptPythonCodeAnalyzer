c8edd8844a0d7f8ccacaa928f6e1b903dd3d0936
stenbror <stenbror@hotmail.com>
2021-02-23 21:49:05 +0100

    Most Atom rule unittests added.

--- __tests__/TestPythonParser.ts
+++ __tests__/TestPythonParser.ts
@@ -6,7 +6,10 @@
 
 import { ASTAtomElipsisNode } from "../src/ast/ASTAtomElipsisNode";
 import { ASTAtomFalseNode } from "../src/ast/ASTAtomFalseNode";
+import { ASTAtomNameNode } from "../src/ast/ASTAtomNameLiteralNode";
 import { ASTAtomNoneNode } from "../src/ast/ASTAtomNoneNode";
+import { ASTAtomNumberNode } from "../src/ast/ASTAtomNumberLiteralNode";
+import { ASTAtomStringNode } from "../src/ast/ASTAtomStringLiteralNode";
 import { ASTAtomTrueNode } from "../src/ast/ASTAtomTrueNode";
 import PythonCoreParser  from "../src/PythonCoreParser";
 import PythonCoreTokenizer  from "../src/PythonCoreTokenizer";
@@ -43,5 +46,29 @@ describe("PythonCoreParser - Expression rules", () => {
         expect(root).toBeInstanceOf(ASTAtomElipsisNode);
         expect(root.getStart()).toBe(0);
         expect(root.getEnd()).toBe(2);
+    });
+    test("Atom literal 'TestVariable_1'", () => {
+        const lex = new PythonCoreTokenizer("TestVariable_1");
+        const parser = new PythonCoreParser(lex);
+        const root = parser.parseAtom();
+        expect(root).toBeInstanceOf(ASTAtomNameNode);
+        expect(root.getStart()).toBe(0);
+        expect(root.getEnd()).toBe(13);
+    });
+    test("Atom literal '0.34J'", () => {
+        const lex = new PythonCoreTokenizer("0.34J");
+        const parser = new PythonCoreParser(lex);
+        const root = parser.parseAtom();
+        expect(root).toBeInstanceOf(ASTAtomNumberNode);
+        expect(root.getStart()).toBe(0);
+        expect(root.getEnd()).toBe(4);
+    });
+    test("Atom literal 'Hello, World'", () => {
+        const lex = new PythonCoreTokenizer("'Hello, World'");
+        const parser = new PythonCoreParser(lex);
+        const root = parser.parseAtom();
+        expect(root).toBeInstanceOf(ASTAtomStringNode);
+        expect(root.getStart()).toBe(0);
+        expect(root.getEnd()).toBe(13);
     });
 });
